ee0e5cd9e2952216c3da3d059256c0aa3a8f64ce
mesterlum <mesterlum@hotmail.com>
2019-07-05 01:06:05 -0600

    It's working

--- process/process.py
+++ process/process.py
@@ -4,8 +4,16 @@ class Maximum:
         self.rout_matrix = rout_matrix
         self.start = start
         self.end = end
-        #sorted(matrix, key=lambda rout: rout["value"], reverse=True)
 
+    def __rest_minimum_value(self, routs):
+        rout_minimum = self.__get_rout_sorted(routs, reverse=False)
+        for rout in routs:
+            # Get index of the rout to rest the minimum
+            index = self.rout_matrix.index(rout) 
+            self.rout_matrix[index]["value"] = self.rout_matrix[index]["value"] - rout_minimum["value"]
+
+        # Finally we will delete the channels
+        self.__delete_channels()
 
     def __get_available_routes(self, rout_lyric):
         sub_routes = []
@@ -15,17 +23,31 @@ class Maximum:
 
         return sub_routes
 
-    def __delete_a_channel(self, rout_from, rout_to):
+    # This method will delete all methods with value 0
+    def __delete_channels(self):
         for rout in self.rout_matrix:
-            if rout["from"] == rout_from and rout["to"] == rout_to:
+            if rout["value"] == 0:
                 self.rout_matrix.remove(rout)
+                print("deleted", rout)
     
-    def __get_better_rout(self, available_routes):
-        return sorted(available_routes, key=lambda rout: rout["value"], reverse=True)[0]
+    def __get_rout_sorted(self, routes, reverse=True):
+        return sorted(routes, key=lambda rout: rout["value"], reverse=reverse)[0]
 
-    def __do_rout(self):
-        sub_routes = self.__get_available_routes(self.start)
-        print(self.__get_better_rout(sub_routes))
+    def __do_rout(self, point, rout = []):
+        sub_routes = self.__get_available_routes(point)
+        better = self.__get_rout_sorted(sub_routes)
+        rout.append(better)
+        if better["to"] != self.end:
+            return self.__do_rout(better["to"], rout)
+        else:
+            return rout
+
+
 
     def resolve(self):
-        self.__do_rout()
+        first = self.__do_rout(self.start)
+        #print
+        self.__rest_minimum_value(first)
+        for x in self.rout_matrix:
+            print(x)
+            
